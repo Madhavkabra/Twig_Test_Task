4e289a8324bf34b4370ebc10d6113c4458fba383
Madhav kabra <madhav.kabra1100@gmail.com>
2023-10-25 19:21:30 +0530

    Cleanup

--- components/Sanitize/index.tsx
+++ components/Sanitize/index.tsx
@@ -12,13 +12,10 @@ export default function Sanitize(props: ISantizeProps): JSX.Element {
   const [cleanHtml, setCleanHtml] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log('==============');
     const clean = DOMPurify.sanitize(props.html, { ADD_ATTR: ['target'] });
 
     setCleanHtml(clean);
   }, []);
-
-  console.log({ cleanHtml });
 
   return cleanHtml ? <Box dangerouslySetInnerHTML={{ __html: cleanHtml }} /> : <></>;
 }
